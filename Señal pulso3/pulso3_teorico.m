9688f13e3823dbaacedfc0bd77db526108e5a14b
paulaquintero2811 <est.paula.vquintero@unimilitar.edu.co>
2026-08-05 00:16:42 -0500

    Add files via upload

---
+++ Señal pulso3/pulso3_teorico.m
@@ -0,0 +1,66 @@
+%% Señal Pulso 3 - Grupo 5
+clear;
+clc;
+close all;
+
+%% Parámetros de la señal
+
+Vp = 3;              % Voltaje pico (V)
+f0 = 2500;           % Frecuencia fundamental (Hz)
+Fs = 2.5e6;          % Frecuencia de muestreo (Hz)
+duty = 30;           % Ciclo útil (%)
+
+%% Tiempo
+
+T = 1/f0;
+t = 0:1/Fs:5*T;
+
+%% Señal de pulso
+
+x = Vp*square(2*pi*f0*t,duty);
+
+%% FFT
+
+N = length(x);
+
+X = fft(x);
+
+P2 = abs(X)/N;
+
+P1 = P2(1:floor(N/2)+1);
+
+P1(2:end-1)=2*P1(2:end-1);
+
+f = Fs*(0:floor(N/2))/N;
+
+%% Señal en el dominio del tiempo
+
+figure
+
+plot(t,x,'LineWidth',1.5)
+
+grid on
+
+xlabel('Tiempo (s)')
+ylabel('Amplitud (V)')
+title('Señal de pulso 3 en el dominio del tiempo')
+
+xlim([0 5*T])
+
+ylim([-3.5 3.5])
+
+%% FFT teórica
+
+figure
+
+stem(f,P1,'filled','LineWidth',1.5)
+
+grid on
+
+xlabel('Frecuencia (Hz)')
+ylabel('Magnitud (V)')
+title('FFT teórica del pulso 3')
+
+xlim([0 25000])
+
+ylim([0 3.5])
